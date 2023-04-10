02a9613c5e3ad1eb24f140e925cf4819c2c1ccf4
otaqulovumidjon <umidjonotaqulov0003@gmail.com>
2023-04-10 16:23:08 +0500

    02

--- count_users_with_age.py
+++ count_users_with_age.py
@@ -8,4 +8,9 @@ def count_users_with_age(data:list, age:int) -> int:
     Returns:
         int: The number of users with the given age
     """
-    return 
+    s = 0
+    for i in data:
+        if i["age"] == age:
+            s += 1
+    return s
+print(count_users_with_age([{"name":"Umidjon", "age":20}, {"name":"Husan", "age":22}], 20))
