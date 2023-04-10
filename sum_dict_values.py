398af3ce65a69839404aa7b8b3a74d7f046ff22b
otaqulovumidjon <umidjonotaqulov0003@gmail.com>
2023-04-10 23:00:44 +0500

    x13

--- sum_dict_values.py
+++ sum_dict_values.py
@@ -6,4 +6,15 @@ def sum_dict_values(data: dict) -> int:
     Returns:
         int: The sum of all values in the dictionary
     '''
-    return 
+    ls = 0
+    for i in data.values():
+        ls += i
+    return ls
+print(sum_dict_values({
+    1: 23, 
+    2: 3.5, 
+    4: 1, 
+    6: 7, 
+    5: 2, 
+    7: 3
+  }))
