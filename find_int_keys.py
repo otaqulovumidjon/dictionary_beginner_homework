a213a8070a44786bff91dd312c3fb71338a9391b
otaqulovumidjon <umidjonotaqulov0003@gmail.com>
2023-04-10 18:26:10 +0500

    x03

--- find_int_keys.py
+++ find_int_keys.py
@@ -15,25 +15,5 @@ def find_int_keys(data: dict) -> list:
     for n in ls:
         if type(n) == int:
             js.append(n)
-    return js
+    return js 
 print(find_int_keys({"x": "23", 3: "y", "z": "5", 7:'a'}))
-
-# x = {
-#     "x": "23",
-#     "3": "y", 
-#     "z": "5", 
-#     7:'a'
-#   }
-# ls = []
-# for i in x.keys():
-#     ls.append(i)
-# s = tuple(ls)
-
-
-# js = []
-# x = 0
-# while x < len(s):
-#     if s[x] == int:
-#         js.append(s[x])
-#     x += 1
-# print(js)
