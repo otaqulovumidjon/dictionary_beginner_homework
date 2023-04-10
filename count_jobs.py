8bb91073b608b2e228f2013bf400e475e8cc35d7
otaqulovumidjon <umidjonotaqulov0003@gmail.com>
2023-04-10 14:46:50 +0500

    x01

--- count_jobs.py
+++ count_jobs.py
@@ -8,4 +8,10 @@ def count_jobs(data:list, job:str) -> int:
     Returns:
         int: The number of users with the given job
     """
-    return 
+    s = 0
+    for i in data:
+        if i['job'] == job:
+            s += 1
+    return s
+print(count_jobs([{'name':'Umidjon', 'job':'dasturchi'}, {'name':'Husan', 'job':'dasturchi'}], 'dasturchi'))
+# print(count_jobs([{'name': 'John', 'job': 'Developer'}, {'name': 'Mary', 'job': 'Developer'}], 'Developer'))
